b36814d30e30c350997807718d294b63a468592d
Michael <michael.patel94@gmail.com>
2022-02-14 19:28:31 -0500

    Adjusting string formatting of hours, minutes, seconds

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 // On load
 window.addEventListener("DOMContentLoaded", function(){
-    // get reference to 'clock' element
-    let clock = document.getElementById("clock");
-    clock.textContent = 1;  // initialize value
-
     // call for update
     updateTime();
 });
@@ -12,20 +8,39 @@ window.addEventListener("DOMContentLoaded", function(){
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 function updateTime(){
+    // get current time
+    let date = new Date();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+
+    // prepend time values with "0" if necessary
+    hours = preprocess_text(hours)
+    minutes = preprocess_text(minutes);
+    seconds = preprocess_text(seconds);
+    
+    // create a string with time values
+    time = hours + ":" + minutes + ":" + seconds;
+
     // get reference to 'clock' element
     let clock = document.getElementById("clock");
 
-    // get time
-
-    //
-
-
-
     // set value for 'clock' element
-    clock.textContent++;
+    clock.textContent = time;
 
     // repeat
     setTimeout(updateTime, 1000);  // milliseconds
+}
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+// prepend time values with "0" if necessary
+function preprocess_text(value){
+    if(value < 10){
+        value = "0" + value;
+    }
+
+    return value;
 }
 
 
